d097e6b91d95104513d851821e99b3608bb789e0
Valentyn <wolf.valentyn@gmail.com>
2022-08-04 14:03:44 +0300

    delete advertising, add dynamic rendering of movies, add numbering

--- js/script.js
+++ js/script.js
@@ -17,10 +17,34 @@
 const movieDB = {
     movies: [
         "Логан",
-        "Лига справедливости",
-        "Ла-ла лэнд",
-        "Одержимость",
-        "Скотт Пилигрим против..."
+        "Ліга справедливості",
+        "Ла-ла ленд",
+        "Одержимість",
+        "Скотт Пілігрим проти..."
     ]
 };
 
+const adv = document.querySelectorAll('.promo__adv img'),
+      poster = document.querySelector('.promo__bg'),
+      genre  = poster.querySelector('.promo__genre'),
+      moviesList = document.querySelector('.promo__interactive-list');
+
+adv.forEach(item => {
+    item.remove();
+});
+
+genre.textContent = 'Драма';
+
+poster.style.backgroundImage = 'url("./img/bg.jpg")';
+
+moviesList.innerHTML = '';
+
+movieDB.movies.sort();
+
+movieDB.movies.forEach((film, i) => {
+    moviesList.innerHTML += `
+        <li class="promo__interactive-item">${i + 1}. ${film}
+            <div class="delete"></div>
+        </li>
+    `;
+});
